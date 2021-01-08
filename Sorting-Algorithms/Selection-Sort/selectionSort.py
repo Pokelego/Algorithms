5cda5eb4b19cdd455cd9c2a529a9d91fdca7a4ee
Jack Parsons <jackpar1113@gmail.com>
2021-01-08 13:39:22 -0700

    add selectionSort.py

---
+++ Sorting-Algorithms/Selection-Sort/selectionSort.py
@@ -0,0 +1,15 @@
+"""
+Selection sort is an algorithm which repeatedly selects the next-smallest element and swap it into place
+
+Time Complexity is O(n^2)
+
+"""
+
+def selection_sort(array):
+   for i in range(len(array)):
+      min_index = i
+      for j in range(i+1, len(array)):
+         if array[j] < array[min_index]:
+            min_index = j
+
+      array[i], array[min_index] = array[min_index], array[i]
