fb0d8d6dc8b21829bb5b98f2b6f808eb3781cb16
Jack Parsons <jackpar1113@gmail.com>
2021-01-20 14:06:00 -0700

    add insertionSort.cpp

---
+++ Algorithms/Sorting-Algorithms/Insertion-Sort/insertionSort.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+
+void insertionSort(std::vector<int>& array){
+   for(int i = 1; i < array.size(); i++){
+      int current = array[i];
+      int j = i - 1;
+      while(j >= 0 && array[j] > current){
+         array[j+1] = array[j];
+         j--;
+      }
+      array[j+1] = current;
+   }
+}
