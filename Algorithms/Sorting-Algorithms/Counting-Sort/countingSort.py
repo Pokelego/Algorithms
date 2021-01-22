66bb1787671fe94953664f0bcb0d0bf890a5ba4b
Jack Parsons <jackpar1113@gmail.com>
2021-01-22 16:05:41 -0700

    add countingSort.py

---
+++ Algorithms/Sorting-Algorithms/Counting-Sort/countingSort.py
@@ -0,0 +1,35 @@
+"""
+Counting sort is an algorithm which finds the max element in an array and creates a new array of size length+1
+Store each elements respective count, then stores a cumulative count of the two arrays into an output array
+
+Time Complexity is O(n + k)
+
+"""
+
+def countingSort(array):
+   # k is the highest possible number to count to
+   k = max(array) + 1
+
+   size = len(array)
+   out = [0] * size
+
+   count = [0] * k
+
+   for i in range(0, size):
+      count[array[i]] += 1
+
+   for i in range(1, k):
+      count[i] += count[i-1]
+
+   i = size -1
+   while i >= 0:
+      out[count[array[i]]-1] = array[i]
+      count[array[i]] -= 1
+      i -= 1
+   for i in range(0, size):
+      array[i] = out[i]
+   
+
+test = [11, 42, 51, 17, 3, 7, 1]
+countingSort(test)
+print(test)
