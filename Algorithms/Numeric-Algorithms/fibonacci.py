e490ce47f9cd16f0342487e30e2bb549bc918fec
Jack Parsons <jackpar1113@gmail.com>
2021-01-27 09:13:34 -0700

    update fibonacci.py

--- Algorithms/Numeric-Algorithms/fibonacci.py
+++ Algorithms/Numeric-Algorithms/fibonacci.py
@@ -1,9 +1,9 @@
 """Calculates the Fibonacci sequence of a given number"""
-memFib = {0:0, 1:1}
+mem_fib = {0:0, 1:1}
 
 
 def fib(n):
-   if n not in memFib:
-      memFib[n] = fib(n-1) + fib(n-2)
-   return memFib[n]
+   if n not in mem_fib:
+      mem_fib[n] = fib(n-1) + fib(n-2)
+   return mem_fib[n]
 
